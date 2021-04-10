16ddc87349d74373c1a16224fad78d311e98bd90
Joe <jmwinitsky@gmail.com>
2021-04-10 16:51:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Note Taker
+
+## Description
+A note-taking app using express and uniqid to write notes, save them in a JSON, and delete them from the JSON.
+
+## Usage
+* When you load the page, click the "get started" button to open the notes interface. 
+* Enter a note title and write something in the body of the note, then save by clicking the save button which appears near the top of the screen.
+* The note will be saved on the server in a JSON file.
+* To delete a note, click the small red delete button next to the note's title in the list of notes on the left.
+
+![A screenshot of the app before clicking the button.](screenshot_1.png)
+![A screenshot of the notes interface.](screenshot_2.png)
---
+++ server.js
@@ -0,0 +1,61 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uniqid = require('uniqid');
+
+var data = fs.readFileSync('./db/db.json', 'utf8'); 
+var db = JSON.parse(data);
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public/index.html'))})
+app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, 'public/notes.html'))})
+
+app.get('/api/notes', (req, res) => {res.sendFile(path.join(__dirname, 'db/db.json'))})
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newNote = req.body;
+
+    newNote.id = uniqid.process()
+    console.log(newNote);
+ 
+    db.push(newNote);
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(db), (err) => {
+        if (err) throw err;
+    })
+    res.json(newNote);
+  });
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+   for (let i = 0; i < db.length; i++) {
+       if (db[i].id === id) {
+           db.splice(i);
+       }
+   }
+
+   fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(db), (err) => {
+    if (err) throw err;
+
+    res.json(true);
+})
+
+})
+
+// listener
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
